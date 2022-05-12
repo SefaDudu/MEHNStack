821c96884cc4e13d3dcd4bab9b8288288b2efe55
Sefa Dudu <77778888+SefaDudu@users.noreply.github.com>
2022-05-12 22:53:27 +0300

    model oluşturuldu

---
+++ model/Todo.js
@@ -0,0 +1,26 @@
+const {Schema,model} = require('mongoose');
+const Adress = new Schema({
+
+    Adress: {
+
+        type:String,
+        required : true
+    },
+    Country: {
+
+        type:String,
+        required : true
+    },
+    City: {
+
+        type:String,
+        required : true
+    },
+
+    completed : {
+        type:Boolean,
+        default:false,
+    }
+})
+
+module.exports=model('Todo',Adress)
