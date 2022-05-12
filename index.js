2cd9ebab2a283783c19a3ea7150bd54b3623be7e
Sefa Dudu <77778888+SefaDudu@users.noreply.github.com>
2022-05-12 20:12:32 +0300

    DB

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log("sefa")
+const express = require("express");
+const mongoose=require("mongoose");
+const app = express();
+const PORT = 3000;
+const uri = "mongodb+srv://sefadudu:dudusefa@cluster0.a6tyc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const hbs = exhbs.create({
+
+    defaultLayout:'main',
+    extname:'hbs'
+    
+    })
+    app.engine('hbs',hbs.engine)
+    app.set('view engine', 'hbs');
+    app.set('views', './views');
+app.use(express.urlencoded({extended:true}));// html body datasını ayıklamaya yarar
+app.use(express.static('public'))
+app.use(todoRoutes);
+async function start(){
+try {
+    await mongoose.connect(uri,{
+        useNewUrlParser:true,
+        useUnifiedTopology:true
+    })
+
+    
+app.listen(PORT,()=>{
+    console.log("Server Running")
+})
+
+} catch (error) {
+    console.log(error)
+}
+
+}
+
+
+start();
